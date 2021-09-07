dd716f4aaf266c1cfd9eb9933204212a88080eb1
mewband12 <mew.chayutaphong@gmail.com>
2021-09-07 19:16:55 +0700

    fix

--- app/javascript/components/ShowModules.jsx
+++ app/javascript/components/ShowModules.jsx
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class ShowModules extends Component {
   render() {
     return (
-      <div> Hello kk </div>
+      <div> <h1>Module</h1> Hello kk </div>
+
+      // modules.keys((module) => {
+      //   <>
+      //     <h2>{module.mod}</h2>
+      //     <p>{module.desc}</p>
+      //   </>
+      // })
     )
   }
 }
