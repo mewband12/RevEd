5c1dcb8333895a51747942f734ffc7dd43248925
mewband12 <mew.chayutaphong@gmail.com>
2021-10-17 19:57:48 +0100

    asd

---
+++ app/javascript/components/Department.jsx
@@ -0,0 +1,18 @@
+// import * as React from 'react';
+import React, { useEffect, useState } from 'react'
+import University from './university/University';
+// const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const theme = createTheme();
+
+const Department = (props) => {
+  // console.log(this.id)
+  const id_uni = props.match.params.id
+  return (
+    <div>
+      <University id ={id_uni}/>
+    </div>
+  )
+}
+
+export default Department
